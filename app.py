914f64dbd9058880b9709c6e9ca54576a7c75b84
breannabisnott <breanna.bisnott@gmail.com>
2024-02-01 22:42:17 -0500

    Question 2

--- app.py
+++ app.py
@@ -12,3 +12,18 @@ def parallel(resistances):
     return(1/r_sum)
 
 print(parallel([2, 5, 3, 5]))
+
+def potential_divider(v_supply, s_resistances):
+    res_sum = 0
+    for r in s_resistances:
+        res_sum = res_sum + r
+
+    current = v_supply/res_sum
+
+    v_res = []
+    for r in s_resistances:
+        v_res.append(current*r)
+
+    print(v_res)
+
+potential_divider(9, [3000, 1000])
